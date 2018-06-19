8cafd5439b1fe1c78c6afcad584bed3d3f23cd0e
Bryan <bws3263827@gmail.com>
2018-06-19 14:34:25 -0400

    Adding generic PROPsEnvironment sml code

---
+++ domains/PROPsInterface/PROPsInterface/src/edu/umich/eecs/soar/propsutil/LearnConfig.java
@@ -0,0 +1,76 @@
+package edu.umich.eecs.soar.propsutil;
+
+public class LearnConfig {
+	private boolean addresses,
+				    proposals,
+				    autos,
+				    conditions,
+				    spreading,
+				    manual;
+	private int chunkThreshold = 2;
+	
+	public LearnConfig(boolean addresses, boolean proposals, boolean autos, boolean conditions, boolean spreading, boolean manual) {
+		this.addresses = addresses;
+		this.proposals = proposals;
+		this.autos = autos;
+		this.conditions = conditions;
+		this.spreading = spreading;
+		this.manual = manual;
+	}
+	public LearnConfig(String str) {
+		if (str.contains("1")) {
+			this.addresses = true;
+		}
+		if (str.contains("2")) {
+			this.proposals = true;
+		}
+		if (str.contains("3")) {
+			this.autos = true;
+		}
+		if (str.contains("c")) {
+			this.conditions = true;
+		}
+		if (str.contains("s")) {
+			this.spreading = true;
+		}
+		if (str.contains("m")) {
+			this.manual = true;
+		}
+	}
+	public LearnConfig(String str, int t) {
+		this(str);
+		chunkThreshold = t;
+	}
+	
+	public void set(boolean addresses, boolean proposals, boolean autos, boolean conditions, boolean spreading, boolean manual) {
+		this.addresses = addresses;
+		this.proposals = proposals;
+		this.autos = autos;
+		this.conditions = conditions;
+		this.spreading = spreading;
+		this.manual = manual;
+	}
+	
+	public void setChunkThreshold(int t) { chunkThreshold = t; }
+	
+	public boolean learnsAddresses() { return addresses; }
+	public boolean learnsProposals() { return proposals; }
+	public boolean learnsAutos() { return autos; }
+	public boolean learnsConditions() { return conditions; }
+	public boolean spreading() { return spreading; }
+	public boolean manual() { return manual; }
+	public int getChunkThreshold() { return chunkThreshold; }
+	
+	@Override
+	public String toString() {
+		String retVal = "";
+		if (addresses) {retVal += "1";}
+		if (proposals) {retVal += "2";}
+		if (autos) {retVal += "3";}
+		if (spreading) {retVal += "s";}
+		if (manual) {retVal += "m";}
+		if (conditions) {retVal += "c";}
+		retVal += "_t" + chunkThreshold;
+		return retVal;
+	}
+}
---
+++ domains/PROPsInterface/PROPsInterface/src/edu/umich/eecs/soar/propsutil/PROPsEnvironment.java
@@ -0,0 +1,465 @@
+package edu.umich.eecs.soar.propsutil;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import sml.Identifier;
+import sml.Kernel;
+import sml.Kernel.UpdateEventInterface;
+import sml.WMElement;
+import sml.smlUpdateEventId;
+import sml.Agent;
+import edu.umich.soar.debugger.SWTApplication;
+
+public abstract class PROPsEnvironment implements UpdateEventInterface {
+	private boolean using_props = true;
+	private boolean verbose = true;
+	private boolean save_percepts = false;
+	private boolean testing = false;
+	protected boolean agentError = false;
+	private String proj_dir = "";
+	private String props_dir = "";
+	
+	protected String agentName = "PROPsAgent";
+	private int agent_num = 0;
+	final protected String CMD_SAY = "say",
+							CMD_FINISH = "finish",
+							CMD_INSTR_NEXT = "props-command",
+							CMD_ERROR = "error";
+	protected String outFileName = "AgentOutput.txt",
+					agent_condchunk_file = "",
+					agent_addresschunk_file = "",
+					agent_instruction_file = "",
+					agent_fetchseq_file = "",
+					agent_genericsoar_file = "";
+	
+	protected LearnConfig currentLearnMode;
+	
+	protected Identifier input_link;
+	protected Kernel kernel;
+	protected Agent agent = null;
+	private WMElement inTask,
+					  inTaskSeqName;
+	
+	protected int numAgentInputs = 3,
+				  numAgentOutputs = 3;
+	private ArrayList<WMElement> inputs;
+	private ArrayList<String> outputs;
+
+
+	protected ArrayList<String> userAgentFiles;
+	protected ArrayList<String> reports;
+
+	//abstract protected void user_initEnvironment();
+	abstract protected void user_createAgent();
+	abstract protected void user_initTask(String task, String taskSeqName);
+	abstract protected void user_doExperiment();
+	abstract protected void user_outputListener(List<String> outputs);
+	abstract protected void user_errorListener(String err);
+	
+	public PROPsEnvironment() {
+		kernel = Kernel.CreateKernelInNewThread();
+		kernel.SetAutoCommit(true);
+		kernel.RegisterForUpdateEvent(smlUpdateEventId.smlEVENT_AFTER_ALL_OUTPUT_PHASES, this, this);
+		
+		userAgentFiles = new ArrayList<String>();
+		reports = new ArrayList<String>();
+		
+		inputs = new ArrayList<WMElement>(numAgentInputs);
+		outputs = new ArrayList<String>(numAgentOutputs);
+		
+		currentLearnMode = new LearnConfig(false, true, false, true, true, true);	// Learn associative combos and conditions by default, using spreading and deliberate fetch sequences
+		
+		//user_initEnvironment();
+		//   User needs to set: 
+		// agentName, PROPS_DIR, PROJ_DIR, 
+		// agent_condchunk_file, agent_addresschunk_file, agent_instruction_file, agent_fetchseq_file, agent_genericsoar_file,
+		// numAgentInputs, numAgentOutputs, 
+		// userAgentFiles
+	}
+
+
+	private void loadLearnProductions(LearnConfig mode) throws Exception {
+		if (mode.spreading() && !mode.learnsConditions()) {
+			if (agent_condchunk_file == "") {
+				throw new Exception("ERROR: User did not provide the condition chunk file!");
+			}
+			agent.LoadProductions(proj_dir + agent_condchunk_file);
+		}
+		if (mode.learnsAutos() && !mode.learnsAddresses() && !mode.learnsProposals()) {
+			agent.LoadProductions(props_dir + "props_learn_l3only.soar");
+			return;
+		}
+		
+		if (!mode.learnsAddresses()) {
+			if (agent_addresschunk_file == "") {
+				throw new Exception("ERROR: User did not provide the addressing chunk file!");
+			}
+			agent.LoadProductions(proj_dir + agent_addresschunk_file);
+		}
+		if (mode.learnsProposals()) {
+			agent.LoadProductions(props_dir + "props_learn_l2.soar");
+		}
+		if (mode.learnsAutos()) {
+			agent.LoadProductions(props_dir + "props_learn_l3.soar");
+		}
+	}
+	
+	public void initAgent() throws Exception {
+		if (agent != null) {
+			if (verbose)
+				agent.ExecuteCommandLine("clog -c");
+			kernel.DestroyAgent(agent);
+		}
+		
+		agent = kernel.CreateAgent(agentName + (agent_num++));
+		agent.SetBlinkIfNoChange(false);
+		agentError = false;
+		
+		input_link = agent.GetInputLink();
+		for (int i=0; i<inputs.size(); ++i) {
+			inputs.set(i, null);
+		}
+		inTask = null;
+		inTaskSeqName = null;
+		
+		if (props_dir == "") {
+			throw new Exception("ERROR: User did not provide the PROPS directory!");
+		}
+		if (proj_dir == "") {
+			throw new Exception("ERROR: User did not provide the domain project directory!");
+		}
+
+		// Load the agent files
+		if (using_props) {
+			if (agent_instruction_file == "") {
+				throw new Exception("ERROR: User did not provide the SMEM instruction file!");
+			}
+			
+			agent.LoadProductions(props_dir + "_firstload_props.soar");			// props library
+			agent.LoadProductions(proj_dir + agent_instruction_file);		// The props instructions
+			
+			if (currentLearnMode.manual()) {
+				if (agent_fetchseq_file == "") {
+					throw new Exception("ERROR: User did not provide the fetch sequence SMEM file!");
+				}
+				agent.LoadProductions(proj_dir + agent_fetchseq_file);	// manual instruction sequences for tasks
+			}
+			if (currentLearnMode.spreading()) {
+				agent.LoadProductions(props_dir + "props_learn_conds.soar");
+			}
+
+			agent.ExecuteCommandLine("chunk confidence-threshold " + currentLearnMode.getChunkThreshold());
+			
+			loadLearnProductions(currentLearnMode);
+		}
+		else if (agent_genericsoar_file != "") {
+			agent.LoadProductions(proj_dir + agent_genericsoar_file);	// editors agent to be learned (for testing)
+		}
+		else {
+			throw new IllegalArgumentException("ERROR: User did not provide the generic Soar agent file!");
+		}
+		
+		loadUserAgentFiles();
+		user_createAgent();
+		
+		if (verbose) {
+			agent.ExecuteCommandLine("watch 1");
+			agent.ExecuteCommandLine("watch --learn 1");
+			agent.ExecuteCommandLine("clog -A verbose_" + outFileName);
+		}
+		else if (!testing){
+			agent.ExecuteCommandLine("watch 0");
+			agent.ExecuteCommandLine("output enabled off");
+			agent.ExecuteCommandLine("output agent-writes off");
+		}
+		
+		if (save_percepts) {
+			agent.ExecuteCommandLine("save percepts -o " + agentName + "Percepts.spr -f");
+			agent.ExecuteCommandLine("save agent " + agentName + "_pre.soar");
+		}
+	}
+	
+	private void loadUserAgentFiles() {
+		// Load the list of user-provided agent files
+		for (String s : userAgentFiles) {
+			agent.LoadProductions(proj_dir + s);
+		}
+		
+	}
+	
+	public void setUserAgentFiles(List<String> filenames) {
+		userAgentFiles = new ArrayList<String>(filenames);
+	}
+
+	public void setCondChunkFile(String filename) { agent_condchunk_file = filename; }
+	public void setAddressChunkFile(String filename) { agent_addresschunk_file = filename; }
+	public void setInstructionsFile(String filename) { agent_instruction_file = filename; }
+	public void setFetchSeqFile(String filename) { agent_fetchseq_file = filename; }
+	public void setSoarAgentFile(String filename) { agent_genericsoar_file = filename; }
+	
+	public void setUseProps(boolean mode) { using_props = mode; }
+	public void setVerbose(boolean mode) { verbose = mode; }
+	public void setSavePercepts(boolean mode) { save_percepts = mode; }
+	
+	public void setPropsDir(String dir) { props_dir = dir; }
+	public void setProjDir(String dir) { proj_dir = dir; }
+	
+	public void setAgentName(String name) { agentName = name; }
+	public void setConfig(LearnConfig config) { currentLearnMode = config; }
+	
+	public void setOutputFile(String filename) { outFileName = filename; }
+	
+	public void setIOSize(int inputSize, int outputSize) {
+		numAgentInputs = inputSize;
+		numAgentOutputs = outputSize;
+		
+		for (int i=0; i<inputs.size(); ++i) {
+			WMElement in = inputs.get(i);
+			if (in != null) {
+				in.DestroyWME();
+				inputs.set(i, null);
+			}
+		}
+		
+		inputs = new ArrayList<WMElement>(numAgentInputs);
+		outputs = new ArrayList<String>(numAgentOutputs);
+		
+		for (int i=0; i<numAgentInputs; ++i) {
+			inputs.add(null);
+		}
+		for (int i=0; i<numAgentOutputs; ++i) {
+			outputs.add(null);
+		}
+	}
+	
+	
+	public void setInput(int slot, String input) throws Exception {
+		if (slot < 0 || slot >= numAgentInputs) {
+			throw new Exception("ERROR: Input slot must be between 1 and " + numAgentInputs);
+		}
+		
+		if (inputs.get(slot) != null) {
+			inputs.get(slot).DestroyWME();
+		}
+		inputs.set(slot, input_link.CreateStringWME("in" + (slot+1), input));
+	}
+	
+	public void initTask(String task, String taskSeqName) {
+		// Clear input if any
+		for (int i=0; i<inputs.size(); ++i) {
+			WMElement in = inputs.get(i);
+			if (in != null) {
+				in.DestroyWME();
+				inputs.set(i, null);
+			}
+		}
+		if (inTask != null) {
+			if (!inTask.GetValueAsString().equals(task)) {
+				inTask.DestroyWME();
+				inTask = null;
+				inTask = input_link.CreateStringWME("task", task);		// for lib-actr to set Gtask
+			}
+		}
+		else 
+			inTask = input_link.CreateStringWME("task", task);
+		if (inTaskSeqName != null) {
+			if (!inTaskSeqName.GetValueAsString().equals(taskSeqName)) {
+				inTaskSeqName.DestroyWME();
+				inTaskSeqName = input_link.CreateStringWME("task-sequence-name", taskSeqName);	// for specifying a known PROPs instruction sequence
+			}
+		}
+		else
+			inTaskSeqName = input_link.CreateStringWME("task-sequence-name", taskSeqName);
+		
+		// Init task
+		user_initTask(task, taskSeqName);
+
+	}
+
+
+	// Clear the output file if it already exists
+	public boolean clearOutputFile() {
+		if (!verbose) {
+			try {
+				PrintWriter writer;
+				writer = new PrintWriter(outFileName);
+				writer.print("");
+				writer.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public void addReport(String r) {
+		reports.add(r);
+	}
+	public void clearReports() {
+		reports.clear();
+	}
+	
+	// Print all current reports
+	public void printReports() {
+		try(FileWriter fw = new FileWriter(outFileName, true);
+				BufferedWriter bw = new BufferedWriter(fw);
+				PrintWriter out = new PrintWriter(bw))
+		{
+			for (String s : reports) {
+				out.println(s);
+				if (verbose)
+					agent.ExecuteCommandLine("echo\t" + s);
+			}
+		}
+		catch (IOException e) {
+			System.err.println("ERROR: Unable to append to file '" + outFileName + "'");
+		}
+	}
+	
+	public void runDebug(String task, String taskSeq) {
+		testing = true;
+		setOutputFile(task + "_" + taskSeq + "_debug_out.txt");
+		try {
+			initAgent();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			return;
+		}
+		clearOutputFile();
+		initTask(task, taskSeq);
+		
+		try {
+			SWTApplication swtApp = new SWTApplication();
+			swtApp.startApp(new String[]{"-remote"});
+			
+			agent.KillDebugger();
+			testing = false;
+			System.exit(0);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		testing = false;
+	}
+	
+	public void runAgent(int steps) {
+		agent.RunSelf(steps);   // Run for the given number of decision cycles
+	}
+	public void runAgent() {
+		agent.RunSelfForever(); // Run until receiving the finish command
+	}
+
+	public void runExperiments(String exp_name, int samples, List<LearnConfig> trials) {
+		// For each parameter combination
+		for (LearnConfig l : trials) {
+			currentLearnMode = l;
+				
+			setOutputFile(exp_name + "_l" + l + "_s" + samples + ".dat");
+			clearOutputFile();
+
+			for (int i=0; i<samples; ++i) {
+				user_doExperiment();
+				System.out.println("\nCompleted sample " + i);
+			}
+
+			if (verbose)
+				agent.ExecuteCommandLine("clog -c");
+				
+			System.out.println("\nCOMPLETED MODE " + l);
+		}
+	}
+	
+	@Override
+	public void updateEventHandler(int arg0, Object arg1, Kernel kernel, int arg3) {
+		
+		if (agentError) {
+			return;
+		}
+		
+		int C = agent.GetNumberCommands();
+		if (C <= 0) {
+			return;
+		}
+		
+		// Note the availability of this function for reporting:  int DC = agent.GetDecisionCycleCounter();
+		// Seemed broken when tried to use it before. Can't ignore impasse DCs in any case.
+
+		boolean stopAgent = false;
+		for (int c = 0; c < C; ++c) {
+			final Identifier id = agent.GetCommand(c);
+			if (id != null && id.IsJustAdded()) {
+				
+				// Handle agent-reported errors
+				if (id.GetCommandName().compareTo(CMD_ERROR) == 0) {
+					agentError = true;
+					stopAgent = true;
+					String msg = id.GetParameterValue("msg");
+					
+					user_errorListener(msg);
+					
+					// Write note to results that they are corrupted
+					try(FileWriter fw = new FileWriter(outFileName, true);
+							BufferedWriter bw = new BufferedWriter(fw);
+							PrintWriter out = new PrintWriter(bw))
+					{
+						out.println("ERROR - AGENT CRASH: " + msg);
+					}
+					catch (IOException e) {
+						System.err.println("ERROR: Unable to append to file '" + outFileName + "'");
+					}
+				}
+				// Handle Soar agent commands
+				else if (id.GetCommandName().compareTo(CMD_SAY) == 0) {
+
+					// Get the output
+					for (int i=0; i<numAgentOutputs; ++i) {
+						outputs.set(i,id.GetParameterValue("out" + Integer.toString(i+1)));
+					}
+					
+					if (outputs.get(0) == null) {
+						System.err.println("WARNING: 'out1' command missing.");
+						id.AddStatusComplete();
+						agent.Commit();
+						break;
+					}
+					
+					// Update the environment using the output
+					user_outputListener(outputs);
+					
+					
+				}
+				else if (id.GetCommandName().compareTo(CMD_FINISH) == 0) {
+					stopAgent = true;
+				}
+				else {
+					id.AddStatusError();
+				}
+				
+				id.AddStatusComplete();
+			}
+		}
+
+		if (stopAgent) {
+			// Clear old input
+			for (int i=0; i<inputs.size(); ++i) {
+				WMElement in = inputs.get(i);
+				if (in != null) {
+					in.DestroyWME();
+					inputs.set(i, null);
+				}
+			}
+			agent.StopSelf();
+			
+			// Reset for next trial
+			user_initTask("", "");
+			
+		}
+	}
+}
